b0249fcf4eb91c8e5e3a613941b3b77261af724e
Michael Galo <michael@michaelthinks.co>
2024-06-27 13:32:22 -0500

    intial notation started

---
+++ src/scripts/Governors.js
@@ -0,0 +1,11 @@
+// import getManager from managers folder
+
+// create export async function to display all governors
+
+// fetch all governors
+
+// map governors and add html template strings, then join
+
+// create handleTargetGovernorChange function
+
+// create event listener to invoke the handleTargetGovernorChange
---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+// import statements
+
+// grab container from the DOM
+
+// create async function for rendering the html to the element above
+
+// invoke the render function
+
+// create event listener for re-rendering the page when transient state is converted into api data
